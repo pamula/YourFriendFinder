cc8b31da910bde6392c8c1d73110cdbe5c957c32
pamula <prathima.amula@gmail.com>
2018-01-02 19:12:19 -0800

    newfriendheroku

---
+++ app/data/friend.js
@@ -0,0 +1,83 @@
+var friendsData = [
+
+  {
+
+    name: "Anushka",
+
+    photo: "http://s3.india.com/wp-content/uploads/2016/07/anushka-sharma123.jpg",
+
+    scores: [3, 4, 3, 3, 4, 4, 4, 1, 5, 3]
+
+  },
+
+  {
+
+    name: "Kareena",
+
+    photo:"http://topyaps.com/wp-content/uploads/2016/11/Kareena-Kapoor.jpg",
+
+    scores: [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+
+  },
+
+  {
+
+    name: "Katrina",
+
+    photo:"http://4.bp.blogspot.com/-yXp_o8zq73c/TgNRvMQednI/AAAAAAAAA5c/5_tlg97MyDQ/s1600/bollywood+actress+katrina+kaif+4.jpg",
+
+    scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+
+  },
+
+  {
+
+    name: "Salman",
+
+    photo:"http://top10xyz.com/wp-content/uploads/2016/05/salman-khan-hd-images-1024x576.jpg",
+
+    scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+
+  },
+
+  {
+
+    name: "Shahrukh",
+
+    photo:"http://wadsam.com/wp-content/uploads/2013/01/Shahrukh-Khan.jpg",
+
+    scores: [5, 4, 4, 4, 4, 3, 2, 1, 5, 5]
+
+  },
+
+  {
+
+    name: "NTR",
+
+    photo:"http://data1.ibtimes.co.in/en/full/591313/junior-ntr.jpg",
+
+    scores: [2, 4, 2, 4, 2, 3, 4, 4, 4, 3]
+
+  },
+
+  {
+
+    name: "Rana",
+
+    photo:"http://www.apnewscorner.com/images/news/2015/05/23/Rana.jpg",
+
+    scores: [5, 4, 5, 4, 5, 3, 3, 3, 2, 4]
+
+  }
+
+];
+
+
+
+
+
+
+
+// Export to API Route
+
+module.exports = friendsData;
